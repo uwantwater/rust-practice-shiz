8de0566701e94ab885e6a1f8326520abb50f34c0
Aradhya Dhungel <97341546+uwantwater@users.noreply.github.com>
2022-07-15 01:02:48 +0000

    Created hello_cargo using cargo build

---
+++ hello_cargo/target/debug/deps/hello_cargo-407008311087c7a4.d
@@ -0,0 +1,5 @@
+/home/runner/rust-practice-shiz/hello_cargo/target/debug/deps/hello_cargo-407008311087c7a4: src/main.rs
+
+/home/runner/rust-practice-shiz/hello_cargo/target/debug/deps/hello_cargo-407008311087c7a4.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/runner/rust-practice-shiz/hello_cargo/target/debug/hello_cargo: /home/runner/rust-practice-shiz/hello_cargo/src/main.rs
